e438b365c833ee49e6cfb4feec47f5935aa4fbd9
Shayan Hosseini <shayan.hosseiny@gmail.com>
2018-12-06 13:26:02 +0330

    create platform sh info

--- cmd/limood/main.go
+++ cmd/limood/main.go
@@ -4,34 +4,32 @@ import (
 	"net/http"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 
 	"github.com/gorilla/mux"
+	psh "github.com/platformsh/gohelper"
 	"github.com/shayanh/limoo-server/application"
 	"github.com/shayanh/limoo-server/track"
 )
 
 func main() {
-	app := application.NewApplication()
+	p, err := psh.NewPlatformInfo()
+	if err != nil {
+		log.Fatal("Not in a Platform.sh Environemnt")
+	}
 
-	// p, err := psh.NewPlatformInfo()
-	// if err != nil {
-	// log.Fatal("Not in a Platform.sh Environemnt")
-	// }
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal(err)
+	}
 
 	router := mux.NewRouter()
 	router.StrictSlash(true)
 
 	track.HandleFuncs(router.PathPrefix("/lyrics").Subrouter())
 
-	// if err := track.InitDB(app.MongoAddr); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer track.CloseDB()
-	// log.Printf("Connected to MongoDB")
-
-	log.Printf("listening on http://%s/", app.ListenAddr)
-	// err = http.ListenAndServe(":"+p.Port, application.Logger(router))
-	err := http.ListenAndServe(app.ListenAddr, application.Logger(router))
+	err = http.ListenAndServe(":"+p.Port, application.Logger(router))
 	if err != nil {
 		log.Fatal(err)
 	}
